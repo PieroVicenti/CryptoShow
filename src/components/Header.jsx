687f139641b683c63eb80c541a2dd45ad11eaeb8
PieroVicenti <p.vicenti@outlook.com>
2022-04-21 23:51:35 +0100

    Crypto project

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { AppBar, Container, createTheme, Menu, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core';
+import React from 'react'
+import {useNavigate} from "react-router-dom";
+import { CryptoState } from '../CryptoContext';
+
+const useStyles = makeStyles(()=>({
+  Title: {
+    flex: 1,
+    color: "#A82BE2",
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    fontFamily: 'Montserrat'
+  }
+}))
+const Header = () => {
+
+  const classes = useStyles();
+  const { currency, setCurrency } = CryptoState();
+  const history = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  })
+
+  return (
+  <ThemeProvider theme={darkTheme}>
+    <AppBar color='transparent' position='static'>
+      <Container>
+        <Toolbar>
+          <Typography onClick={()=> history("/")} className={classes.Title} variant="h6">
+            CryptoMania
+          </Typography>
+          <Select value={currency} onChange={(e) => setCurrency(e.target.value)} variant='outlined' style={{width: 100, height: 40, marginRight: 15}}>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"GBP"}>GBP</MenuItem>
+            <MenuItem value={"EUR"}>EUR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  </ThemeProvider>
+  )
+};
+
+export default Header;
